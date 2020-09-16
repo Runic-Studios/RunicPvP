f92fdc2c6fe54f2077ed0021f22263b7a744b6c0
Skyfallin <dimetrius.hightower@gmail.com>
2020-09-16 09:33:51 -0700

    Initial Commit

---
+++ src/main/java/com/runicrealms/plugin/RunicPvP.java
@@ -0,0 +1,17 @@
+package com.runicrealms.plugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class RunicPvP extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
