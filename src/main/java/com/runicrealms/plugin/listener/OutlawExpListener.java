293e2e2aae24bbb74131831120d7b9f84a966c7b
Excel619 <excel8392@gmail.com>
2023-06-05 16:21:50 -0600

    Updated to use new combat exp system

--- src/main/java/com/runicrealms/plugin/listener/OutlawExpListener.java
+++ src/main/java/com/runicrealms/plugin/listener/OutlawExpListener.java
@@ -1,7 +1,7 @@
 package com.runicrealms.plugin.listener;
 
 import com.runicrealms.plugin.RunicPvP;
-import com.runicrealms.plugin.events.RunicExpEvent;
+import com.runicrealms.plugin.events.RunicCombatExpEvent;
 import com.runicrealms.plugin.rdb.RunicDatabase;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
@@ -13,12 +13,11 @@ import org.bukkit.event.Listener;
 public class OutlawExpListener implements Listener {
 
     @EventHandler(priority = EventPriority.NORMAL)
-    public void onExpGain(RunicExpEvent event) {
+    public void onExpGain(RunicCombatExpEvent event) {
         int slot = RunicDatabase.getAPI().getCharacterAPI().getCharacterSlot(event.getPlayer().getUniqueId());
         if (!RunicPvP.getAPI().isOutlaw(event.getPlayer(), slot)) return;
         if (event.isCancelled()) return;
-        if (event.getRunicExpSource() != RunicExpEvent.RunicExpSource.MOB) return; // only mobs
-        int bonus = (int) (event.getOriginalAmount() * RunicPvP.getOutlawBonusExpPercent());
-        event.setFinalAmount(event.getFinalAmount() + bonus);
+        if (event.getRunicExpSource() != RunicCombatExpEvent.RunicExpSource.MOB) return; // only mobs
+        event.setBonus(RunicCombatExpEvent.BonusType.OUTLAW, RunicPvP.getOutlawBonusExpPercent());
     }
 }
