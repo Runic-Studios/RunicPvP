e2836c96897a0baaab4e84be7568687b66c8d4db
Skyfallin <dimetrius.hightower@gmail.com>
2023-06-30 16:14:45 -0400

    Outlaw bonus now applies to gathering

--- src/main/java/com/runicrealms/plugin/listener/OutlawExpListener.java
+++ src/main/java/com/runicrealms/plugin/listener/OutlawExpListener.java
@@ -2,6 +2,7 @@ package com.runicrealms.plugin.listener;
 
 import com.runicrealms.plugin.RunicPvP;
 import com.runicrealms.plugin.events.RunicCombatExpEvent;
+import com.runicrealms.plugin.professions.event.RunicGatheringExpEvent;
 import com.runicrealms.plugin.rdb.RunicDatabase;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
@@ -13,7 +14,7 @@ import org.bukkit.event.Listener;
 public class OutlawExpListener implements Listener {
 
     @EventHandler(priority = EventPriority.NORMAL)
-    public void onExpGain(RunicCombatExpEvent event) {
+    public void onCombatExpGain(RunicCombatExpEvent event) {
         int slot = RunicDatabase.getAPI().getCharacterAPI().getCharacterSlot(event.getPlayer().getUniqueId());
         if (!RunicPvP.getAPI().isOutlaw(event.getPlayer(), slot)) return;
         if (event.isCancelled()) return;
@@ -21,4 +22,14 @@ public class OutlawExpListener implements Listener {
         if (event.getPlayer().getWorld().getName().equalsIgnoreCase("dungeons")) return; // no outlaw bonus in dungeons
         event.setBonus(RunicCombatExpEvent.BonusType.OUTLAW, RunicPvP.getOutlawBonusExpPercent());
     }
+
+    @EventHandler(priority = EventPriority.NORMAL)
+    public void onGatheringExpGain(RunicGatheringExpEvent event) {
+        int slot = RunicDatabase.getAPI().getCharacterAPI().getCharacterSlot(event.getPlayer().getUniqueId());
+        if (!RunicPvP.getAPI().isOutlaw(event.getPlayer(), slot)) return;
+        if (event.isCancelled()) return;
+        if (event.getPlayer().getWorld().getName().equalsIgnoreCase("dungeons")) return; // no outlaw bonus in dungeons
+        event.setBonus(RunicGatheringExpEvent.BonusType.OUTLAW, RunicPvP.getOutlawBonusExpPercent());
+    }
+
 }
