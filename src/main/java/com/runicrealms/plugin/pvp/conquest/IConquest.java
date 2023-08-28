a0ef7bc4d328a4c9ebed3869b92baab9a588a8e8
BoBoBalloon <boboballoongaming@gmail.com>
2023-08-27 21:54:02 -0400

    Loot api deprecated

--- src/main/java/com/runicrealms/plugin/pvp/conquest/IConquest.java
+++ src/main/java/com/runicrealms/plugin/pvp/conquest/IConquest.java
@@ -1,12 +1,14 @@
 package com.runicrealms.plugin.pvp.conquest;
 
-import com.runicrealms.plugin.item.lootchests.LootChest;
+import com.runicrealms.plugin.loot.chest.LootChest;
 import io.lumine.mythic.api.mobs.MythicMob;
 
 public interface IConquest {
 
     CapturePoint getCapturePoint(); // radius for now, update to grid to be more accurate
+
     MythicMob getMythicMob();
+
     LootChest getLootChest();
 
     void spawnLootChest();
